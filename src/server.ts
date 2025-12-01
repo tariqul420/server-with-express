a991986d70c743069204612b77bad2119411de7c
tariqul420 <tariqul@tariqul.dev>
2025-12-01 18:24:51 +0600

    Add PostgreSQL integration with database setup and update dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from "express";
 import config from "./config";
+import db from "./config/db";
 const app = express();
 
 // parser
 app.use(express.json());
+
+// init db
+db();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
